ac00ffa5f77f9822e0c71779bae44e407a9a7eb1
asy224 <asy224@nate.com>
2022-01-15 20:03:43 +0900

    Create project

---
+++ .postcssrc.js
@@ -0,0 +1,22 @@
+// 파일 앞에 . 표시는 구성옵션이나 숨김파일일떄 사용하는 방법
+// ESM
+// CommonJS (node.js 환경)
+
+
+// import 
+// require()
+
+// export
+
+// export {
+//     plugins : [
+
+//     ]
+// }
+
+
+module.exports = {
+    plugins: [
+        require('autoprefixer')
+    ]
+}
